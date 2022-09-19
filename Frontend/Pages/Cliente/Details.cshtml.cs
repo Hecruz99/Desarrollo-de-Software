edc6a4598a68b443a83c76a57e430bd9f8be3ef8
hecruz99 <hecruz99@hotmail.com>
2022-09-18 22:00:38 -0500

    Detalles Paginas

--- Frontend/Pages/Cliente/Details.cshtml.cs
+++ Frontend/Pages/Cliente/Details.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using persistencia;
+using Dominio;
 
 
 namespace MyApp.Namespace
 {
-    public class DetailsClienteModel : PageModel
+ public class DetailsClienteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioCliente _repo;
+
+        public Cliente Cliente { get; set;}
+
+        public DetailsClienteModel(IRepositorioCliente repositorioCliente)
         {
+            _repo = repositorioCliente;
+        }
+
+        public void OnGet(int id)
+        {
+            Cliente = _repo.Get(id);
         }
     }
 }
--- Frontend/Pages/Tecnico/Details.cshtml.cs
+++ Frontend/Pages/Tecnico/Details.cshtml.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using persistencia;
+using Dominio;
 
 
 namespace MyApp.Namespace
 {
-    public class DetailsTecnicoModel : PageModel
+public class DetailsTecnicoModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioTecnico _repo;
+
+        public Tecnico Tecnico { get; set;}
+
+        public DetailsTecnicoModel(IRepositorioTecnico repositorioTecnico)
         {
+            _repo = repositorioTecnico;
+        }
+
+        public void OnGet(int id)
+        {
+            Tecnico = _repo.Get(id);
         }
     }
 }
